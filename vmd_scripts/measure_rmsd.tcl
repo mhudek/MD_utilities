2ffc8d5d40d6d4c497343646b0bc77b40e0b164d
mhudek <43149008+mhudek@users.noreply.github.com>
2022-02-04 15:37:34 +0000

    Create measure_rmsd.tcl

---
+++ vmd_scripts/measure_rmsd.tcl
@@ -0,0 +1,32 @@
+Usage: vmd -dispdev text -e measure_rmsd.tcl 
+
+set win 1
+set in "win$win.dcd"
+
+## load files here
+
+mol new ../../input/cht_sc.psf
+mol addfile ../../input/cht_sc.pdb
+mol addfile $in waitfor all
+
+
+## define output
+
+set out [open "rmsd_win$win.dat" w]
+puts $out "#Frame       rmsd"
+
+## define selections and measure rmsd
+set ref [atomselect top "fragment 36" frame 0]
+set sel [atomselect top "fragment 36"]
+
+for {set i 0} {$i < [molinfo top get numframes]} {incr i 1} {
+
+   $sel frame $i
+
+   set rmsd [measure rmsd $sel $ref]
+   puts $out "$i        $rmsd"
+}
+
+close $out
+
+quit
