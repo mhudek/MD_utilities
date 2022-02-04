08302b4fe5d757c2fdb8c1c012566fd1c6714c71
mhudek <43149008+mhudek@users.noreply.github.com>
2022-02-04 10:55:46 +0000

    Create namd_energy.sh

---
+++ namd/namd_energy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# extract energies from NAMD log file and prepare for plotting
+#Usage: namd_energy.sh in_file out_file
+
+in_file=$1
+out_file=$2
+
+grep -m 1 "ETITLE" $in_file > tmp.txt
+grep "ENERGY" $in_file |awk -v OFS=", " '$1=$1' >> tmp.txt
+grep -v "Info" tmp.txt > tmp2.txt
+
+sed 's/ENERGY:,//g' tmp2.txt > tmp3.txt
+sed 's/ENERGY://g' tmp3.txt > $out_file
+
+rm tmp.txt
+rm tmp2.txt
+rm tmp3.txt
