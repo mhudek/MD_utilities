48c663a5680b8d338f615d062b50540ba2a86b81
mhudek <43149008+mhudek@users.noreply.github.com>
2022-02-28 15:06:03 +0000

    Create get_pbc_boxsize.tcl

---
+++ vmd_scripts/get_pbc_boxsize.tcl
@@ -0,0 +1,31 @@
+# get pbc box size from .dcd simulation
+
+proc get_box_size {} {
+    set outfile1 [open "box_size.csv" w+] 
+    puts $outfile1 "#step, x, y, z"
+
+    set box [pbc get -all]
+    set nf [llength $box]
+    
+
+    set n 0 
+    while {$n<$nf} {
+        set b [lindex $box $n] 
+        set bl [split $b  ]
+
+        set x [lindex $bl 0]
+        set y [lindex $bl 1]
+        set z [lindex $bl 2]
+
+        puts $outfile1 "$n, $x, $y, $z  "
+    
+        incr n
+    }   
+    puts $n
+    # foreach {$box} [puts $outfile1 "$box \n"]
+    
+    #puts $outfile1 $box(2)
+    close $outfile1
+}
+
+get_box_size
