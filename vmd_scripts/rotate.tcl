e9b4e0c20f28944bbb70c34056f7efb54e9058ac
mhudek <43149008+mhudek@users.noreply.github.com>
2022-02-28 14:29:22 +0000

    Create rotate.tcl

---
+++ vmd_scripts/rotate.tcl
@@ -0,0 +1,27 @@
+# rotates all atoms around y-axis by 90 deg
+
+proc rotate {in_psf in_pdb out_pfx } { 
+
+    resetpsf
+    readpsf $in_psf
+    coordpdb $in_pdb
+
+    mol load psf $in_psf pdb $in_pdb
+
+
+    set all [atomselect top all ]
+    set matrix [transaxis y 90] 
+
+    $all move $matrix
+
+    foreach atom [$all get {segid resid name x y z}] {
+            foreach {segid resid name x y z} $atom { break }
+            coord $segid $resid $name [list $x $y $z] 
+    }   
+
+
+    writepsf $out_pfx.psf
+    writepdb $out_pfx.pdb
+
+    mol delete top 
+}
