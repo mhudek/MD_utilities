a5e47ed14cf8f5e27375e0346eee0e7668647838
mhudek <43149008+mhudek@users.noreply.github.com>
2022-10-13 13:17:33 +0100

    Update fix_chitosan.tcl

--- namd/fix_chitosan.tcl
+++ namd/fix_chitosan.tcl
@@ -1,5 +1,7 @@
+# Create .pdb file to fix molecule
+
 set all [ atomselect top all ]
 set fix [ atomselect top "fragment 12 to 17" ]
-$all set occupancy 0
-$fix set occupancy 1
-$all writepdb fix_cht_middle.pdb
+all set occupancy 0
+fix set occupancy 1
+all writepdb fix_cht_middle.pdb
