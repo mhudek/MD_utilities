61c4441ae3464db3941729345cfa4c4830bb6ffd
mhudek <43149008+mhudek@users.noreply.github.com>
2022-10-25 13:32:42 +0100

    Create write_px.tcl

---
+++ vmd_scripts/write_px.tcl
@@ -0,0 +1,19 @@
+#Script for creating .pdb and .psf in VMD containing only selected residues. 
+#Author: Magdalena Hudek
+#Usage: vmd -dispdev text -e write_px.tcl
+
+#Inpuut names without extension
+set input "cht_sc"
+set output "cht6"
+
+mol new ${input}.psf
+mol addfile ${input}.pdb
+
+readpsf ${input}.psf
+coordpdb ${input}.pdb
+
+#Define selection here
+set sel [atomselect top "fragment 36 and segname CHT"]
+
+$sel writepsf $output.psf
+$sel writepdb $output.pdb
