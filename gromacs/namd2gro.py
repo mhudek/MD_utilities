d8d40b20228e13f5fc7047ac12c1ae79531c8113
mhudek <43149008+mhudek@users.noreply.github.com>
2022-10-25 10:01:23 +0100

    Update namd2gro.py

--- gromacs/namd2gro.py
+++ gromacs/namd2gro.py
@@ -1,8 +1,13 @@
 import parmed as pmd
 
+#Load .psf and .pdb 
 psf = pmd.load_file('step3_input.psf')
 psf.coordinates = pmd.load_file('step3_input.pdb').coordinates
+
+#Load all necessary parameter files (CHARMM toppar format)
 params = pmd.charmm.CharmmParameterSet('par_interface.prm','water.prm')
 psf.load_parameters(params)
+
+#Define output
 psf.save('out.top') # .top is recognized as a GROMACS topology file
 psf.save('out.gro')
