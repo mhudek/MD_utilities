7c825080a54f3de16a1495e3f8daa906864ca2f9
mhudek <43149008+mhudek@users.noreply.github.com>
2023-02-28 10:38:58 +0000

    Update create_wham_files.sh

--- umbrella_sampling/GROMACS/create_wham_files.sh
+++ umbrella_sampling/GROMACS/create_wham_files.sh
@@ -13,11 +13,11 @@ do
 		echo "umbrella${i}.tpr" > tpr-files.dat
 	else
 		cp ../output/$i/umbrella${i}_pullx.xvg .
-    cp ../output/$i/umbrella${i}_pullf.xvg .
-    cp ../output/$i/umbrella${i}.tpr .
+    		cp ../output/$i/umbrella${i}_pullf.xvg .
+    		cp ../output/$i/umbrella${i}.tpr .
 
 		echo "umbrella${i}_pullx.xvg" >> pullx-files.dat
-    echo "umbrella${i}_pullf.xvg" >> pullf-files.dat
-    echo "umbrella${i}.tpr" >> tpr-files.dat
+    		echo "umbrella${i}_pullf.xvg" >> pullf-files.dat
+    		echo "umbrella${i}.tpr" >> tpr-files.dat
 	fi
 done
