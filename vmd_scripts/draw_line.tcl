c91c3f4d83e46286c934691970eb69beec6eb95d
mhudek <43149008+mhudek@users.noreply.github.com>
2023-03-01 13:37:05 +0000

    Update draw_line.tcl
    Draw a line between surface and polymer end & display current simulation time.

--- vmd_scripts/draw_line.tcl
+++ vmd_scripts/draw_line.tcl
@@ -1,34 +1,43 @@
-#!bin/tcl
+proc vmd_draw_arrow {mol start end} {
+    # an arrow is made of a cylinder and a cone
+    set middle [vecadd $start [vecscale 0.9 [vecsub $end $start]]]
+    set pos [ list [lindex $middle 0] 0.0 [lindex $middle 2] ]
+    set dist [lindex [vecsub $end $start] 2]
+    set dist_t [format "%.2f" $dist]
+    graphics $mol cylinder $start $middle radius 0.3
+    graphics $mol cone $middle $end radius 0.4
+    graphics $mol text $pos "$dist_t" size 1 thickness 3
+}
 
-proc drawline{} {
+proc update_frame_number {args} {
 
-set f [molinfo top get numframes]
+  set frame [molinfo 0 get frame]
+  puts "Frame: $frame "
+  #in this case timestep was 2 ps 
+  set t [format "%.2f" [expr $frame * 0.002]]
 
-set all [ atomselect top all ]
-set c2s [ atomselect top "name C2 and fragment 0 to 5"]
-set c4  [ atomselect top "name C4 and resid 361" ]
+  set surf [ atomselect top "charge '-0.9' '-0.675' and resid 1 to 135"]
+  set c4  [ atomselect top "serial 10510" ]
 
-set c2com [ measure center $c2s]
+  set c2com [ measure center $surf]
+  foreach {xcom ycom zcom} $c2com { break }
 
-foreach {xcom ycom zcom} $c2com { break }
+  set xc4 [$c4 get x]
+  set yc4 [$c4 get y]
+  set zc4 [$c4 get z]
 
-set xc4 [$c4 get x]
-set yc4 [$c4 get y]
-set zc4 [$c4 get z]
+  set p1 [list $xc4 $yc4 $zcom]
+  lassign [$c4 get {x y z}] p2
 
-set p1 [list $xc4 $yc4 $zcom]
-lassign [$c4 get {x y z}] p2
+  graphics 0 delete all
 
-graphics 0 line $p1 $p2
+  draw color blue
+  draw arrow $p1 $p2
+  draw color black
+  draw text {5 0 -10} "Time $t ns" size 1 thickness 3
 
 }
 
-proc enabletrace{} {
-global vmd_frame
-trace variable vmd_frame([molinfo top]) w drawline
-}
+# Register the update_frame_number function to be called every time the frame changes
+trace add variable vmd_frame(0) write "update_frame_number"
 
-proc disabletrace{} {
-global vmd_frame
-trace vdelete vmd_frame([molinfo top]) w drawline
-}
